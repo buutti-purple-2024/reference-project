27f48c808e6490f081cd90c45a0983fd36e1dff9
SusuHoo <susu.hallberg@gmail.com>
2024-04-26 15:16:07 +0300

    Clean out the post-file

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -1,8 +1,6 @@
 import "./post.scss";
 //import { mdiBorderAllVariant } from '@mdi/js';
 import { mdiDotsHorizontal } from '@mdi/js';
-import { mdiArrowDownBoldOutline } from '@mdi/js';
-import { mdiArrowUpBoldOutline } from '@mdi/js';
 import { mdiMessageOutline } from '@mdi/js';
 import { mdiDragVertical } from '@mdi/js';
 import Icon from '@mdi/react';
@@ -24,23 +22,16 @@ interface PostItem {
     };
   }
 
-
 const Post: React.FC<PostItem> = ({post}) => {
-    /* const [liked, setLiked] = useState(false); 
-
-    
-    const toggleLike = () => {
-        setLiked(!liked);
-    }; */
 
     const [ commentOpen, setCommentOpen ] = useState(false)
 
-    const liked = false;
-
     return (
-        <div className="post">
-            <div className="container">
-            <div className="user">          
+      <div className="post">
+          <div className="container">
+
+            <div className="user">
+
                 <div className="userInfo">
                     <img src={post.profilePic} alt="" />
                     <div className="details">
@@ -50,33 +41,27 @@ const Post: React.FC<PostItem> = ({post}) => {
                             <span className="name">{post.name}</span>
                             <Icon path={mdiDragVertical} size={1} />
                         </Link>
-                        
                     </div>
                 </div>
+                
                 <span className="date" justify-content="none">1 min ago</span>
                 {/* <Icon path={mdiBorderAllVariant} size={1} /> */}
                 <Icon path={mdiDotsHorizontal} size={1} />
-                
             </div>
+
             <div className="content">
                 <img src={post.img} alt=""></img>
                 <p>{post.desc}</p>
             </div>
-            <div className="item-arrow">
-          <div className={liked ? "boldArrow" : ""}>
-            <Icon path={mdiArrowUpBoldOutline} size={1} />
-          </div>
-          <div className={!liked ? "boldArrow" : ""}>
-            <Icon path={mdiArrowDownBoldOutline} size={1} />
-          </div>
-          </div>
+
             <div className="info" onClick={()=>setCommentOpen(!commentOpen)}>
-            <Icon path={mdiMessageOutline} size={1} /> 10 comments
+              <Icon path={mdiMessageOutline} size={1} /> 10 comments
             </div>
-            {<Comments />}
+            
+            <Comments />
             <VotingButtons/>
-            </div>
-        </div>
+          </div>
+      </div>
     );
 };
 
