af593794e5a893a2b904ffb1c7cf75336909aadd
Lena <83163194+lemi35@users.noreply.github.com>
2024-05-13 01:11:36 +0300

    chat updated

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -1,9 +1,47 @@
-import "./chat.scss"
+import "./chat.scss";
+import fakeUsers from "../../tempData/fakeUsers";
+//import MessageType from "../../types/MessageType";
+import fakeMessages from "../../tempData/fakeMessages";
+import ChatBanner from "../../components/chat banner/ChatBanner";
+import Messages from "../../components/messages/Messages";
 
 const Chat = () => {
+    
+    
+    // temp data
+    const user = {
+        id: 1,
+        username: 'TestUser',
+        password: 'password',
+        role: 'user',
+        token: null,
+        tokenExpire: null,
+        createdAt: new Date().toISOString(),
+        profileText: "this is profile text",
+        bannerImage: "https://images.pexels.com/photos/17045504/pexels-photo-17045504/free-photo-of-cute-gray-kitten.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        profileImage: "https://images.pexels.com/photos/18866393/pexels-photo-18866393/free-photo-of-woman-wearing-straw-hat-on-a-field-in-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        posts: 2,
+        follows: 6
+      }
+
+      
+    
+
     return (
-        <div className="chat">Chat messages here</div>
-    )
-}
+        <div className="chat-cont">
+            <ChatBanner
+                username={user.username}
+                profileImage={user.profileImage}
+            />
+            <hr />
+
+            <div className="chat-messages">
+            
+                <Messages messages={fakeMessages} setMessages={() => {}} users={fakeUsers} />
+            </div>
+        </div>
+    
+    );
+};
 
 export default Chat;
