4a784168651cc34f95a87e183bbc48121393d7ec
Arttu Haverinen <arttuhav@student.uef.fi>
2024-05-13 14:59:55 +0300

    add cookies, testing authentication and roles in frontend

--- backend/middleware/adminCheckMiddleware.ts
+++ backend/middleware/adminCheckMiddleware.ts
@@ -4,15 +4,19 @@ import type { JwtPayload } from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+const generateAccessToken = (user : {name : string} ) => {
+	if(process.env.ACCESS_TOKEN_SECRET) return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15m"});
+};
 
 export const adminCheckMiddleware = async (req:Request,res:Response,next:NextFunction) => {
-	const authenticationHeader = req.headers["authorization"];
-	if (authenticationHeader == null) return res.status(401).send();
-	const token = authenticationHeader.split(" ")[1];
+	const accessToken = req.cookies.accesstoken;
+	//console.log("admin middleware", accessToken);
+	const token = accessToken; 
+	//console.log(token);
 	if (process.env.ACCESS_TOKEN_SECRET) 
 		try {
 			const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) as JwtPayload;
-			console.log(decoded);
+			//console.log(decoded);
 			const isAdmin = await prisma.user.findUnique({
 				where: {
 					username: decoded.name
@@ -20,19 +24,57 @@ export const adminCheckMiddleware = async (req:Request,res:Response,next:NextFun
 			});
 			if (isAdmin) {
 				if (isAdmin.role == "admin") {
-					console.log(isAdmin.role);
+					//console.log(isAdmin.role);
 					next();
 				}
-        else {
-          res.json(403).send();
-        }
+				else {
+					res.json(403).send();
+				}
 			}
-      else {
-        res.json(403).send();
-      }
+			else {
+				res.json(403).send();
+			}
 		} catch (error) {
-			//console.log(error);
-			res.status(403).send();
+			if (req.cookies.refreshtoken) {
+				//console.log("access token decoding failed, checking refresh token validity");
+				try {
+					const user = await prisma.user.findFirst({
+						where: {
+							token : req.cookies.refreshtoken
+						}
+					});
+					if (user) {
+						const newAccessToken = generateAccessToken( {name: user.username});
+						//console.log(newAccessToken);
+
+						const isAdmin = await prisma.user.findUnique({
+							where: {
+								username: user.username
+							}
+						});
+						if (isAdmin) {
+							if (isAdmin.role == "admin") {
+								//console.log(isAdmin.role);
+								res.cookie("accesstoken", newAccessToken, {maxAge: 24 * 60 * 60 * 1000, httpOnly: false});
+								next();
+							}
+							else {
+								res.json(403).send();
+							}
+						}
+						else {
+							res.json(403).send();
+						}
+					} else {
+						res.json(403).send();
+					}
+				}	
+				catch (error) {
+					//console.log("error1");
+					res.status(401).send();
+				}
+			}
+
 		}
 };
 
