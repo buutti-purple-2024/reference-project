88dae99728f9096391d2d49f93a41a4ec5049742
Arttu Haverinen <arttuhav@student.uef.fi>
2024-05-22 13:08:00 +0300

    fix merge conflict in profilebanner component

--- frontend/src/components/profileBanner/profileBanner.tsx
+++ frontend/src/components/profileBanner/profileBanner.tsx
@@ -11,12 +11,7 @@ interface BannerProps {
 const ProfileBanner: React.FC<BannerProps> = ({ username, profileBanner, profileImage, profileText }) => {
     return (
         <div className="banner">
-<<<<<<< HEAD
-            {console.log(username)}
-            <img src={bannerImage} alt="Banner" className="banner-image" />
-=======
             <img src={profileBanner} alt="Banner" className="banner-image" />
->>>>>>> 0077a152c56d63cbcba5e0c070d1dd3bbcf74222
             <img src={profileImage} alt="ProfileImage" className="profile-image" />
             <div className="username">My Profile: {username}</div>
             <div className="profiletext">{profileText}</div>
