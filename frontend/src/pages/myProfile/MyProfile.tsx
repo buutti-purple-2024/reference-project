f24d07c35946017cf930c124224614e8347c538d
SusuHoo <susu.hallberg@gmail.com>
2024-06-04 15:04:18 +0300

    fix myProfile-bug, update seed.ts

--- frontend/src/pages/myProfile/MyProfile.tsx
+++ frontend/src/pages/myProfile/MyProfile.tsx
@@ -3,7 +3,7 @@ import "./myProfile.scss"
 import ChangePassword from "../../components/changePassword/ChangePassword";
 import CreateTopic from "../../components/createTopic/CreateTopic";
 import DeleteAccount from "../../components/deleteAccount/DeleteAccount";
-import ProfileContent from "../../components/profileContent/ProfileContent";
+//import ProfileContent from "../../components/profileContent/ProfileContent";
 import UserType from "../../types/UserType";
 import CurrentUserProfile from "../../components/profile/CurrentUserProfile";
 
@@ -13,14 +13,14 @@ interface ProfileProps {
 }
 
 const Profile: React.FC<ProfileProps> = ({currentUser}) => {
-    let userIsLoggedIn = document.cookie.split("; ").find((row) => row.startsWith("refreshtoken"))?.split("=")[1]
+    const userIsLoggedIn = document.cookie.split("; ").find((row) => row.startsWith("refreshtoken"))?.split("=")[1]
     
     if (!currentUser) {
         return <div>No user logged in</div>;
   }
     return (
         <div className="profilePage">
-            <ProfileContent currentUser={currentUser}/>
+            <CurrentUserProfile currentUser={currentUser}/>
             <CreateTopic/>
             { userIsLoggedIn && <ChangePassword/>}
             { userIsLoggedIn && <DeleteAccount/>} 
