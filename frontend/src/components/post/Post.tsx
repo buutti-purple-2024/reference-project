e8f5f10b22e0e55e4bee3234513f31d58d6c76da
miikamoii <miimoi@hotmail.com>
2024-06-05 04:12:50 +0300

    Update Post.tsx
    - now renders both external and local post images correctly

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -1,7 +1,5 @@
 import "./post.scss";
-import { mdiDotsHorizontal } from '@mdi/js';
-import { mdiMessageOutline } from '@mdi/js';
-import { mdiDragVertical } from '@mdi/js';
+import { mdiDotsHorizontal, mdiMessageOutline, mdiDragVertical } from '@mdi/js';
 import Icon from '@mdi/react';
 import Comments from "../comments/Comments";
 import VotingButtons from "../votingButtons/votingButtons";
@@ -9,9 +7,8 @@ import { Link } from "react-router-dom";
 import { useState, useContext } from "react";
 import PostType from "../../types/PostType";
 import UsersContext from "../../contexts/UsersContext";
-import {CommentsProvider} from "../../contexts/CommentsContext";
+import { CommentsProvider } from "../../contexts/CommentsContext";
 import WriteComment from "../comments/WriteComment";
-
 
 interface PostProps {
   post: PostType;
@@ -23,64 +20,61 @@ interface PostProps {
 }
 
 const Post: React.FC<PostProps> = ({ post, username, profileImage, upvotes, downvotes }) => {
-  
-    const [ commentOpen, setCommentOpen ] = useState(false);
-   // const {comments} = useContext(CommentsContext);
-    const users = useContext(UsersContext);
+  const [commentOpen, setCommentOpen] = useState(false);
+  const users = useContext(UsersContext);
 
-    const toggleCommentSection = () => {
-      setCommentOpen(!commentOpen);
-    };
+  const toggleCommentSection = () => {
+    setCommentOpen(!commentOpen);
+  };
 
+  const getImageUrl = (image: string | null) => {
+    if (!image) {
+      return '';
+    }
+    const isExternalUrl = image.startsWith('http://') || image.startsWith('https://');
+    return isExternalUrl ? image : `http://localhost:3001/${image}`;
+  };
 
-    return (
-      <div className="post">
-          <div className="postContainer">
-            <div className="postInfo">
-                <div className="userInfo">
-                    {profileImage && <img src={profileImage} alt="" />}
-                    {username}
-                    <div className="details" >
-                        <Link 
-                            to={`/profile/${post.user_id}`} 
-                            style={{textDecoration:"none", color:"inherit"}}>
-                            <Icon path={mdiDragVertical} size={1} />
-                        </Link>
-                    </div>
-                </div>
-
-                <span className="date" justify-content="none">{post.created_at}</span>
-                <Icon path={mdiDotsHorizontal} size={1} />
+  return (
+    <div className="post">
+      <div className="postContainer">
+        <div className="postInfo">
+          <div className="userInfo">
+            {profileImage && <img src={profileImage} alt="" />}
+            {username}
+            <div className="details">
+              <Link to={`/profile/${post.user_id}`} style={{ textDecoration: "none", color: "inherit" }}>
+                <Icon path={mdiDragVertical} size={1} />
+              </Link>
             </div>
-
-            <div className="postContent">
-            <img src={post.image || ''} alt=""/>
-                <h5>{post.title}</h5> 
-                <p>{post.content}</p>   
-            </div>
-
-            <div className="info"> 
-              <VotingButtons upvotes={upvotes} downvotes={downvotes} post_id={post.post_id} />
-              <div className="comment" onClick={toggleCommentSection}>
-                  <Icon path={mdiMessageOutline} size={1} /> {commentOpen ? "Hide" : "Show"} comments
-              </div>
-            </div> 
-
-          <div>
-            {commentOpen && (
-              <div className="comments-container">
-                <CommentsProvider postId={post.post_id}>
-                  <Comments postId={post.post_id} users={users} />
-                </CommentsProvider>
-              </div>
-            )}
           </div>
-          
-          <div>
-              <WriteComment postId={post.post_id}/>
+          <span className="date">{post.created_at}</span>
+          <Icon path={mdiDotsHorizontal} size={1} />
+        </div>
+
+        <div className="postContent">
+          {post.image && <img src={getImageUrl(post.image)} alt="Post Image" />}
+          <h5>{post.title}</h5>
+          <p>{post.content}</p>
+        </div>
+
+        <div className="info">
+          <VotingButtons upvotes={upvotes} downvotes={downvotes} post_id={post.post_id} />
+          <div className="comment" onClick={toggleCommentSection}>
+            <Icon path={mdiMessageOutline} size={1} /> {commentOpen ? "Hide" : "Show"} comments
           </div>
-            
+        </div>
+
+        {commentOpen && (
+          <div className="comments-container">
+            <CommentsProvider postId={post.post_id}>
+              <Comments postId={post.post_id} users={users} />
+            </CommentsProvider>
           </div>
+        )}
+
+        <WriteComment postId={post.post_id} />
+      </div>
     </div>
   );
 };
